aebed8da400032489895a037272138c3dfebc8bf
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-02-28 21:35:28 -0500

    base_model.py update

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@
 """
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """ BaseModel Class definition.
@@ -53,11 +53,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def save(self):
         """ Stores the modifcation date and time of the object.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ Returns a dictionary containing all key/values of the instance.
