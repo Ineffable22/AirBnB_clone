7374d1460061d4796d757d7090ebd1665d9f4fe4
MiguelBarreraDev <barreram987@gmail.com>
2022-03-01 17:12:33 -0500

    Feature: new methods to handle emptylines, help and breakpoints

--- console.py
+++ console.py
@@ -1,26 +1,48 @@
 #!/usr/bin/python3
+""" Module
+        pass
+"""
 import cmd
+import os
+import pwd
+
 
 class HBNBCommand(cmd.Cmd):
-    intro = ("*" * 25) +\
-        "\n* Welcome to my Shell\n*" +\
-        "\n* Authors:\n*" +\
-        "\tMiguel Barrera\n*" +\
-        "\tMiguel Grillo\n*" +\
+    """ Class definition
+            pass
+    """
+    intro = ("\033[38;2;245;97;166m*" * 25) +\
+        "\n* Welcome {}\n".format(pwd.getpwuid(os.getuid()).pw_name) +\
         ("*" * 25)
-    prompt = "(HBNB)$"
+    prompt = "\033[38;2;255;56;92m(HBNB)$\033[0m "
 
     def emptyline(self):
         pass
+
     def do_EOF(self, line):
+        """ Terminates the running program
+        Usage:
+        (HBNH)$ 'ctrl + D'
+        """
+        print()
         return True
+
     def do_quit(self, line):
-        """Quit command to exit the program"""
+        """ Quit command to exit the program
+        Usage:
+        (HBNB)$ quit
+        """
         return True
+
     def do_help(self, arg):
-        'List available commands with "help" or detailed help with "help cmd".'
+        """ Help for commands
+        Usage:
+        (HBNB)$ help // List available commands
+        (HBNB)$ help 'cmd' // Detailed help on the command(cmd)
+        """
+        # 'Listavailablecommands with "help" or detailed help with "help cmd"'
         cmd.Cmd.do_help(self, arg)
-            
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
