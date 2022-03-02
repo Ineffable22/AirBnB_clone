3cfecfe3716ac93450cca4962c636ffe34d72673
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-02 18:40:54 -0500

    Feature(__init__): new classes was added Place, State, City, Amenity and Review

--- models/__init__.py
+++ models/__init__.py
@@ -4,10 +4,20 @@
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 cls_dict = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "Place": Place,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Review": Review
 }
 
 storage = FileStorage()
