cc001dc457afc735b85c0864b0567ad2f2bcb2bc
MiguelBarreraDev <barreram987@gmail.com>
2022-03-05 10:19:09 -0500

    Features(test/test_model): test_base_model test file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Class definition
+"""
+from models.base_model import BaseModel
+import unittest
+
+
+class TestModules(unittest.TestCase):
+    """
+    Class definition
+    """
+    def test_uper(self):
+        """
+        Method definition
+        """
+        self.assertEqual(5, 3)
