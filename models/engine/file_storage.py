8a4ce6ab19fb0cb1c499afd41df8dee0aa13de70
MiguelBarreraDev <barreram987@gmail.com>
2022-03-06 06:17:18 -0500

    Update(file_storage.py): Use cls_dict imported from __init__.py in models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,23 @@
 """
 import json
 import models
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+cls_dict = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "Place": Place,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Review": Review
+}
 
 
 class FileStorage:
