94270009d83c8bfe702f08cf2592fa141c45938d
MiguelBarreraDev <barreram987@gmail.com>
2022-03-06 06:52:38 -0500

    Change(models): Change of the position to the cls_dict.v2

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,23 @@
 """ Define the parent directory as a package for the python interpreter.
 """
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+cls_dict = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "Place": Place,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Review": Review
+}
 
 storage = FileStorage()
 storage.reload()
