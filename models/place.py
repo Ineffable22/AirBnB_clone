c8942edce5a08127e9a79c3a6b6b6dfc5bf62ee8
MiguelBarreraDev <barreram987@gmail.com>
2022-03-06 06:01:52 -0500

    Fix(console.py): Remove write error

--- models/place.py
+++ models/place.py
@@ -9,8 +9,8 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    numbers_rooms = 0
-    numbers_bathrooms = 0
+    number_rooms = 0
+    number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
     latitude = 0.0
