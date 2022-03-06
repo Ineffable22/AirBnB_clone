ace1444ca31fdc8b2cda05f90b286f671d2b8b3d
MiguelBarreraDev <barreram987@gmail.com>
2022-03-05 22:00:44 -0500

    Add: Comments in docstring format for all methods

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" TESTS """
+""" TESTS Module """
 import unittest
 from models.base_model import BaseModel
 
 
 class TESTStringMethods(unittest.TestCase):
     """ TESTStringMethods Class """
+
     def test_upper(self):
+        """ Test definition """
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
+        """ Test definition """
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
     def test_split(self):
+        """ Test definition """
         s = 'hello world'
         self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
