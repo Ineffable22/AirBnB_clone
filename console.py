dade47a676ffe95c06108a48f0e8a247ea2d1ac7
MiguelBarreraDev <barreram987@gmail.com>
2022-03-06 04:41:53 -0500

    Fix(console.py): Format value of the attribute in update commad

--- console.py
+++ console.py
@@ -43,7 +43,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, line):
         """Updates an instance(add or set attribute)"""
-        parts = line.split()
+        parts = line.split(" ", 3)
+        if re.search("^\".*\\s?.*\"", parts[3]):
+            print("We have match bro")
+            parts[3] = re.findall("^\".*\\s?.*\"", parts[3])[0]
+            print(parts[3])
         if self.check_conditions(parts, 4):
             setattr(
                 storage.all()[parts[0] + "." + parts[1]],  # object
