6f0cde9418109b6f9bc23c9686da382e5ade775f
MiguelBarreraDev <barreram987@gmail.com>
2022-03-05 22:30:49 -0500

    Update(base_model.py): Add id if it’s a new instance

--- models/base_model.py
+++ models/base_model.py
@@ -46,7 +46,6 @@ class BaseModel:
             kwargs: dict, optional
                 Contains key/values to add to the instance
         """
-        self.id = str(uuid.uuid4())
         if kwargs is not None and kwargs != {}:
             for key, value in kwargs.items():
                 if key in ["created_at", "updated_at"] and type(value) is str:
@@ -54,6 +53,7 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
