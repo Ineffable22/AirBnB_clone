ef18290d5e698607ceb9bee11b3266b0df3391a1
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-12 15:30:27 -0500

    Add: 3-header.css

---
+++ web_static/styles/3-header.css
@@ -0,0 +1,15 @@
+header {
+    background-color: white;
+    height: 70px;
+    width: 100%;
+    border-bottom: 1px solid #CCCCCC;
+}
+
+.logo {
+    background-image: url("../images/logo.png");
+    height: 100%;
+    width: 142px;
+    margin-left: 20px;
+    background-repeat: no-repeat;
+    background-position: center;
+}
